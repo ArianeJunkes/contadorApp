cb7ccbbd3cbf95ba266882d6ed7fcdef26f6d3a2
ArianeJunkes <arianejunkes312@gmail.com>
2023-03-14 16:22:41 -0300

    feito o limitador no incrementar

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Counter />
-      <Counter />
-      <Counter />
+      <Counter limite={10} />
     </View>
   );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,5 @@
 import { Component } from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
-import { TextInput } from 'react-native-web';
+import { Button, Text, View, TextInput, StyleSheet } from 'react-native';
 
 export default class Counter extends Component {
     state = {
@@ -8,7 +7,9 @@ export default class Counter extends Component {
         step: 2,
     };
     incrementar() {
+        if (this.state.count < this.props.limite){
         this.setState({ count: this.state.count + this.state.step });
+    }
     }
     decrementar() {
         this.setState({ count: this.state.count - 1 });
@@ -22,11 +23,11 @@ export default class Counter extends Component {
     render() {
         return (
             <View style={styles.contador}>
-                <TextInput value={this.state.step}
+                <TextInput value={this.state.step.toString()}
                  onChange={(e) => this.alteraStep(e)}
                  inputMode="numeric"
                  keyboardType='numeric' />
-                 <TextInput value={this.state.step}
+                 <TextInput value={this.state.step.toString()}
                  onChangeText={(text) => this.alteraStep2(text)}
                  inputMode="numeric"
                  keyboardType='numeric' />
@@ -42,5 +43,5 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alingItems: 'center',
     },
-   
+   }
 );
